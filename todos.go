4c0aa19ab9258ed7a0dd97e11dbb7db0edf2b099
Utkarsh Nagar <utkarshnagar2000@gmail.com>
2023-10-09 11:19:55 +0000

    update added

--- todos.go
+++ todos.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-    "bufio"
-    "strings"
-    "strconv"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func help(){
-	output_str:= `
+func help() {
+	output_str := `
 
                 WELCOME TO TODOS
     ----------------------------------------------------------------
@@ -23,169 +23,221 @@ func help(){
     3. Delete a todo                    rm "Todo_Index"
     3. Update a todo                    update "Todo_Index" new_todo
     4. Mark a todo as done              done "Todo_Index"
-    5. Show all the completed todos     ls_done "Todo_Index"
 	`
 
 	fmt.Println(output_str)
 }
 
-func readFile() ([]byte,error){
-    file,err := os.Open("todos.json")
+func readFile() ([]byte, error) {
+	file, err := os.Open("todos.json")
 
-    if(err != nil){
-        fmt.Println("error = ",err)
-    }
+	if err != nil {
+		fmt.Println("error = ", err)
+	}
 
-    data,error := ioutil.ReadAll(file)
-    file.Close()
+	data, error := ioutil.ReadAll(file)
+	file.Close()
 
-    if error != nil {
-        fmt.Println("Error:", error)
-        return nil, error // Return the error
-    }
+	if error != nil {
+		fmt.Println("Error:", error)
+		return nil, error // Return the error
+	}
 
-    return data,nil
+	return data, nil
 }
 
 func writeToFile(updatedData []byte) {
-    if err := ioutil.WriteFile("todos.json", updatedData, 0644); err != nil {
-        fmt.Println("Error writing file:", err)
-        return 
-    }
-    return
+	if err := ioutil.WriteFile("todos.json", updatedData, 0644); err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+	return
 }
 
-func addTask(task string){
-    data,error := readFile()
+func addTask(task string) {
+	data, error := readFile()
 
-    if error != nil {
-        fmt.Println("Error reading file:", error)
-        return
-    }
+	if error != nil {
+		fmt.Println("Error reading file:", error)
+		return
+	}
 
-    var contentMap map[string]interface{}
-    json.Unmarshal([]byte(data),&contentMap)
+	var contentMap map[string]interface{}
+	json.Unmarshal([]byte(data), &contentMap)
 
-    key := strconv.Itoa(len(contentMap) + 1)
-    contentMap[key] = task
+	key := strconv.Itoa(len(contentMap) + 1)
+	contentMap[key] = task
 
-    updatedData,err := json.Marshal(contentMap)
+	updatedData, err := json.Marshal(contentMap)
 
-    if err != nil {
-        fmt.Println("Error marshaling JSON:", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
 
-    writeToFile(updatedData)
+	writeToFile(updatedData)
 
-    fmt.Println("Task Added")
+	fmt.Println("Task Added")
 }
 
-func deleteTask(taskKey string){
-    data,error := readFile()
+func deleteTask(taskKey string) {
+	data, error := readFile()
 
-    if error != nil {
-        fmt.Println("Error reading file:", error)
-        return
-    }
+	if error != nil {
+		fmt.Println("Error reading file:", error)
+		return
+	}
 
-    var contentMap map[string]interface{}
-    json.Unmarshal([]byte(data),&contentMap)
-    fmt.Println(contentMap," ",taskKey)
-    delete(contentMap,taskKey)
-    newContentMap := make(map[string]interface{})
+	var contentMap map[string]interface{}
+	json.Unmarshal([]byte(data), &contentMap)
+	delete(contentMap, taskKey)
+	newContentMap := make(map[string]interface{})
 
-    index :=1 
+	index := 1
 
-    for _,value := range(contentMap) {
-        newKey := strconv.Itoa(index)
-        newContentMap[newKey] = value
-        index++
-    }
+	for _, value := range contentMap {
+		newKey := strconv.Itoa(index)
+		newContentMap[newKey] = value
+		index++
+	}
 
-    updatedData,err := json.Marshal(newContentMap)
+	updatedData, err := json.Marshal(newContentMap)
 
-    if err != nil {
-        fmt.Println("Error marshaling JSON:", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
 
-    writeToFile(updatedData)
-    fmt.Println("Task Deleted")
+	writeToFile(updatedData)
+	fmt.Println("Task Deleted")
 }
 
-func updateTask(task string){
-    data,error := readFile()
+func updateTask(taskIndex int, task string) {
+	data, error := readFile()
 
-    if error != nil {
-        fmt.Println("Error reading file:", error)
-        return
-    }
+	if error != nil {
+		fmt.Println("Error reading file:", error)
+		return
+	}
 
-    var contentMap map[string]interface{}
-    json.Unmarshal([]byte(data),&contentMap)
+	var contentMap map[string]interface{}
+	json.Unmarshal([]byte(data), &contentMap)
+	taskIndexString := strconv.Itoa(taskIndex)
+	contentMap[taskIndexString] = task
+
+	updatedData, err := json.Marshal(contentMap)
+
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	writeToFile(updatedData)
+
+	fmt.Println("Task Updated")
 
 }
 
-func completedTasks(task string){}
+func completedTasks(task string) {}
 
-func markTaskAsDone(task string){}
+func markTaskAsDone(taskKey string) {
+	data, error := readFile()
 
-func showActiveTaskList(){
-    file,err := os.Open("todos.json")
+	if error != nil {
+		fmt.Println("Error reading file:", error)
+		return
+	}
 
-    if(err != nil){
-        fmt.Println("error = ",err)
-    }
+	var contentMap map[string]interface{}
+	json.Unmarshal([]byte(data), &contentMap)
+	delete(contentMap, taskKey)
+	newContentMap := make(map[string]interface{})
 
-    data,_ := ioutil.ReadAll(file)
-    file.Close()
+	index := 1
 
-    var contentMap map[string]interface{}
-    json.Unmarshal([]byte(data),&contentMap)
+	for _, value := range contentMap {
+		newKey := strconv.Itoa(index)
+		newContentMap[newKey] = value
+		index++
+	}
 
-    // output content
-    fmt.Println("--------------------------------")
-    fmt.Println("ACTIVE TASKS")
-    fmt.Println("--------------------------------")
-    for key,value := range contentMap{
-        fmt.Println(key,value)
-    }
+	updatedData, err := json.Marshal(newContentMap)
+
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	writeToFile(updatedData)
+	fmt.Println("Task Completed 🎉")
 }
 
-func main(){
-    reader := bufio.NewReader(os.Stdin)
+func showActiveTaskList() {
+	file, err := os.Open("todos.json")
+
+	if err != nil {
+		fmt.Println("error = ", err)
+	}
+
+	data, _ := ioutil.ReadAll(file)
+	file.Close()
+
+	var contentMap map[string]interface{}
+	json.Unmarshal([]byte(data), &contentMap)
+
+	// output content
+	fmt.Println("--------------------------------")
+	fmt.Println("ACTIVE TASKS")
+	fmt.Println("--------------------------------")
+	for key, value := range contentMap {
+		fmt.Println(key, value)
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
 
 	help()
 
-    fmt.Println("Enter command to continue....")
-    fmt.Println()
+	fmt.Println("Enter command to continue....")
+	fmt.Println()
 
-    userInput, _ := reader.ReadString('\n')
-    userInput = strings.TrimSpace(userInput)
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.TrimSpace(userInput)
 
-    actions := strings.SplitN(userInput," ",2)
+	actions := strings.SplitN(userInput, " ", 2)
 
-    
-    switch actions[0]{
-        case "add":
-            task := actions[1]
-            addTask(task)
-        case "ls":
-            showActiveTaskList()
-        case "rm":
-            task := actions[1]
-            deleteTask(task)
-        case "update":
-            task := actions[1]
-            updateTask(task)
-        case "ls_done":
-            task := actions[1]
-            completedTasks(task)
-        case "done":
-            task := actions[1]
-            markTaskAsDone(task) 
-        default:
-            fmt.Println("Enter command to continue....")
-    }
+	switch actions[0] {
+	case "add":
+		task := actions[1]
+		addTask(task)
+	case "ls":
+		showActiveTaskList()
+	case "rm":
+		taskKey := actions[1]
+		deleteTask(taskKey)
+	case "update":
+		showActiveTaskList()
+		fmt.Println("Enter Index of Task to update")
+		var taskIndex int
+		_, err := fmt.Scanf("%d", &taskIndex)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Print("Enter Task: ")
+
+		newTask, _ := reader.ReadString('\n')
+		newTask = strings.TrimSpace(newTask)
+		updateTask(taskIndex, newTask)
+	// case "ls_done":
+	//     task := actions[1]
+	//     completedTasks(task)
+	case "done":
+		showActiveTaskList()
+		fmt.Print("Enter Index of Task: ")
+		taskKey, _ := reader.ReadString('\n')
+		markTaskAsDone(taskKey)
+	default:
+		fmt.Println("Enter command to continue....")
+	}
 }
